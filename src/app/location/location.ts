e29c05bcfcead9a57015a789a2e21805534c60bf
Silva <wesley.silva@br.experian.com>
2025-09-12 18:08:24 -0300

    Conversao IA

--- src/app/location/location.ts
+++ src/app/location/location.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-location',
+  standalone: true,
   imports: [],
   templateUrl: './location.html',
   styleUrl: './location.scss'
